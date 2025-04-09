7fd00d2e3af42778a8348a7d5aeafcb981e5d15f
Allan Lond <158562268+Jamarare@users.noreply.github.com>
2025-04-09 16:13:49 +0300

    Lesson 2

---
+++ Test.py
@@ -0,0 +1,8 @@
+class Task:
+    def _init_(self,title,status = "To-do"):
+        self.title = title
+        self.status = status
+        
+    def mark_done(self):
+        self_status = "done"
+    
